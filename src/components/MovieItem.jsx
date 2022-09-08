6a01b98249e339f7cf510f0b7cfb9129613b1de1
Adeline64 <adeline.lacabanne@gmail.com>
2022-09-08 21:11:49 +0200

    faire la partie MovieItem : création du useState, création de toggleFavorite avec true et false, ajout du onclick qui est égal à toggleFavorite et ajout de la condition ternaire favorite pour déterminé si le film est ds les favoris ou non

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -5,15 +5,31 @@ function MovieItem(props) {
   const { title, released, director, poster } = props;
 
   // create a "favorite" state, default value : false
+  const [favorite, setfavorite] = useState(false)
 
   // and add an action when clicking on the favorite button
 
+  function toggleFavorite() {
+    // alert("ca marche"); le alert fonctionne
+    // console.log(favorite);
+    if (favorite === false) {
+      setfavorite(true)
+      // console.log(favorite);
+    }else{
+      setfavorite(false)
+      // console.log(favorite);
+    }
+    console.log(favorite);
+  }
+  
   return (
-    <div class='MovieItem'>
+    <div className='MovieItem'>
       <h2>{title}</h2>
       <h3>Director: {director}</h3>
       <h5>Released: {released}</h5>
-      <button type='button'>Add to favorites</button>
+      <button onClick={toggleFavorite} type='button'>
+        {favorite ? "Enlever des favoris" : "Ajouter aux favoris"}
+      </button>
       <div>
         <img src={poster} alt={title} />
       </div>
